372ac309bd9c9cd8725041fea4918edb28da7c24
Александр <shlykovn@mail.ru>
2017-10-05 15:59:28 +0300

    save mobile

--- app/System/src/Body/BodyScript.php
+++ app/System/src/Body/BodyScript.php
@@ -11,14 +11,13 @@ use Kinomania\System\MobileDetect\MobileDetect;
 
 class BodyScript {
 	public static function getContent(){
-		$mobile = new MobileDetect();
-		if($mobile->isMobile()) {
-			?>
+		$mobile = new MobileDetect();?>
+		<?php if($mobile->isMobile()):?>
 			<script src="//static.videonow.ru/vn_init.js?profileId=2785734" defer></script><!--mobile-->
-			<?php } else {?>
-				<script src="//data.videonow.ru/?profile_id=103919&format=vast.."></script>
-				<script src="//static.videonow.ru/vn_init.js?profileId=2786448" defer></script>
-			<?php
-		}
+		<?php else:?>
+			<script src="//data.videonow.ru/?profile_id=103919&format=vast.."></script>
+			<script src="//static.videonow.ru/vn_init.js?profileId=2786448" defer></script>
+		<?php endif;?>
+<?php
 	}
 }
