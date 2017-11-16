f685e1e1fe2771fe11a4b13e69fd0792c4baaff6
Александр <shlykovn@mail.ru>
2017-11-16 22:26:10 +0300

    player

--- app/Original/view/index/ssi/trailer.html.php
+++ app/Original/view/index/ssi/trailer.html.php
@@ -609,7 +609,6 @@
 <script src="http://fs.kinomania.ru/app/js/video.js"></script>
 <script src="http://fs.kinomania.ru/app/js/videojs.ads.js"></script>
 <script src="http://fs.kinomania.ru/app/js/videojs-preroll.js"></script>
-<script type="text/javascript" src="<?= $static ?>/app/js/film.js"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('.video-prewiew').click(function(){
